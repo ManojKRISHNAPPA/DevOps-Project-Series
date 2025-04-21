3fa1d869207915fe77c529f4c8591e4d006087fe
Manoj-K <k_manoj@apple.com>
2025-04-21 21:22:00 +0530

    updated the shell scripting file:

---
+++ Terraform/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = ""
+    secret_key = "" 
+}
+
+
+resource "aws_instance" "admin" {
+    ami = "ami-084568db4383264d4"
+    instance_type = "t2.medium"
+    security_groups = [ "default" ]
+    key_name = "project"
+    root_block_device {
+      volume_size = 20
+      volume_type = "gp3"
+      delete_on_termination = true
+    }
+    tags = {
+        Name = "Admin-server"
+    }
+
+}
+
+
+output "PublicIP" {
+  value = aws_instance.admin.public_ip
+}
